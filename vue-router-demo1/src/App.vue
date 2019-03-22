ead8ceb1d9ae0e92827f6abd9ed92dd09549bd74
kaixin1013 <2413890705@qq.com>
2019-03-22 15:18:39 +0800

    new

---
+++ vue-router-demo1/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<div id="app">
+ <div class="header">
+ 	<router-link to="/" class="nav-item" >
+		<img src="./assets/logo.png"/>
+		</router-link>
+ 	<router-link to="/task" class="nav-item">任务中心</router-link>
+	<router-link to="/lib" class="nav-item">库管理</router-link>
+	<router-link to="/ucenter" class="nav-item">王恺鑫</router-link>
+ </div>
+ <div class="contentainer content"><router-view></router-view></div>
+ 
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+	#app {
+		background-color: rgb(244,244,244);
+	}
+	
+	.header{
+		display: flex;
+		height: 100px;
+		background-color: ghostwhite;
+		font-size: 20px;
+		padding-left: 120px;
+		align-items: center;
+	}
+	.nav-item{
+		width: 100px;
+		margin-right: 20px;
+	
+	}
+	.container{
+		width:80%;
+		margin: 0 auto;
+		height: 400px;
+		
+	}
+
+a {
+	text-decoration: none;
+	color: #aaa;
+}
+
+a:hover{
+	
+	color: rgb(2,165,218);
+}
+
+</style>
---
+++ vue-router-demo1/src/components/UCenter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="container">
+		<h2>个人中心</h2>
+		{{msg}}
+	</div>
+	
+</template>
+
+<script>
+	export default{
+		name:'UCenter',
+		data(){
+			return{
+				
+			};
+		}
+	};
+</script>
+
+<style scoped>
+</style>
